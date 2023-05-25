fae93bbf6fae7a606509a2b89335675254d77117
MARLON ABRIL <maabril56@soy.sena.edu.co>
2023-05-25 14:43:18 -0500

    proyecto 20

--- static/sccrip.js
+++ static/sccrip.js
@@ -1,11 +1,16 @@
-const open_btn = document.querySelector('.open-btn')
-const close_btn = document.querySelector('.close-btn')
-const nav = document.querySelectorAll('.nav')
-
-open_btn.addEventListener('click', () => {
-    nav.forEach(nav_el => nav_el.classList.add('visible'))
-})
-
-close_btn.addEventListener('click', () => {
-    nav.forEach(nav_el => nav_el.classList.remove('visible'))
-})
+(function($) {
+    $(document).ready(function() {
+        const open_btn_menu = document.querySelector('.open-btn')
+        const close_btn = document.querySelector('.close-btn')
+        const nav = document.querySelectorAll('.nav')
+        
+        open_btn_menu.addEventListener('click', () => {
+            nav.forEach(nav_el => nav_el.classList.add('visible'))
+        })
+        
+        close_btn.addEventListener('click', () => {
+            nav.forEach(nav_el => nav_el.classList.remove('visible'))
+        })
+        
+    });
+})(jQuery);
